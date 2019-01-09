cbf43847e4bfb739b1c84f2fe2da80cd5497eb3c
thanhtungbn <bkflying.tung@gmail.com>
2019-01-10 00:02:27 +0700

    initial commit

---
+++ WCF_WebService/EmployeeService/EmployeeService/EmpInfoService.svc.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace EmployeeService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "EmpInfoService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select EmpInfoService.svc or EmpInfoService.svc.cs at the Solution Explorer and start debugging.
+    public class EmpInfoService : IEmpInfoService
+    {
+        public string GetEmpSalary(string EmpId)
+        {
+            return "Salary of" + EmpId + " is" + 4000;
+        }
+    }
+}
---
+++ WCF_WebService/EmployeeService/EmployeeService/IEmpInfoService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace EmployeeService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IEmpInfoService" in both code and config file together.
+    [ServiceContract]
+    public interface IEmpInfoService
+    {
+        [OperationContract]
+        [WebInvoke(Method = "GET", UriTemplate = "/Tung/{EmpId}", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Wrapped)]
+        string GetEmpSalary(string EmpId);
+    }
+}
